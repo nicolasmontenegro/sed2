9addd136a1111206b872904a95271737044c24c8
kotick <max.jvl@gmail.com>
2014-07-08 18:20:04 -0400

    pintando pajaritos felices

--- main_one.cpp
+++ main_one.cpp
@@ -10,6 +10,7 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+#include <uBarrier.h>
 
 #define PI 3.14159265
 #define EJEX 640
@@ -18,91 +19,28 @@
 
 using namespace std;
 
-
-/*_Task Pajaro;
-
-
-_Monitor Mapa
-{
-	vector < vector < Pajaro* > > mapa;
-	vector < Pajaro* > lista;
-	uCondition mapaTomado;
-
-public:
-	Mapa()
-	{
-		mapa.resize(EJEX);
-		for (int i = 0; i < EJEX; ++i)
-		{
-			vector < Pajaro* > ejeY (EJEY);
-			mapa[i]=ejeY;
-		}
-
-	};
-	~Mapa(){};
-
-	void nacer (Pajaro *aux)
-	{
-		this->lista.push_back(aux);	
-		cout << "pio ctm!" << endl;
-	};
-
-	bool anadir (int posX, int posY, Pajaro *aux)
-	{
-		if (!mapaTomado.empty())
-			mapaTomado.wait();
-
-		mapaTomado.signalBlock();
-
-		if (mapa[posX][posY]!=NULL)
-			return false;
-		mapa[posX][posY]=aux;
-
-		mapaTomado.signal();
-
-		return true;
-	};
-
-	bool eliminar (int posX, int posY, Pajaro *aux)
-	{
-		if (!mapaTomado.empty())
-			mapaTomado.wait();
-
-		mapaTomado.signalBlock();
-
-		if (mapa[posX][posY]==NULL || mapa[posX][posY]!=NULL)
-			return false;
-		mapa[posX][posY]=NULL;
-		
-		mapaTomado.signal();
-		
-		return true;
-	};
-
-};*/
-
-
-
 _Task Pajaro
 {
-	//Mapa *cielo;
-	float posX, posY, angulo;
+	float posX, posY, angulo, velocidad, aceleracion;
+
 
 protected:
 	
 	void main ()
 	{
 		//cielo->nacer(this);
-		cout << "Creada id: " << this << " X = " << posX << " Y = " << posY << " Ang = " << angulo  << endl;
+		cout << "Creada id: " << this << " X = " << posX << " Y = " << posY << " Ang = " << angulo << " Vel = " << velocidad << endl;
 		//cout << "rad " << ( angulo * PI / 180.0 ) << endl;
 		//cielo->anadir(posX, posY, this);
 
 
 		while(1)
 		{
-			auxposX = posX;
-			auxposY = posY;
-			auxAngulo = angulo;
+
+			// Separacion
+			// Cohecion
+			// Alineacion
+
 			float movX = pow (sin ( angulo * PI / 180.0 ), 2);
 			if (angulo < 360 && angulo > 180)
 				movX *= -1.0;
@@ -110,32 +48,36 @@ protected:
 			if (angulo > 90 && angulo < 270)
 				movY *= -1.0;
 
-			posX+=movX;
-			if (posX >= EJEX-1.0)
+			posX+=(movX*velocidad);
+			if (posX >= EJEX)
 				posX -= EJEX;
 			else if (posX <= 0)
 				posX += EJEX-1.0;
 
-			posY+=movY;		
+			posY+=(movY*velocidad);	
 			if (posY <= 0)
 				posY += EJEY-1.0;
-			else if (posY >= EJEY-1.0)
+			else if (posY >= EJEY)
 				posY -= EJEY;
-		
-			//control.eliminar(auxposX, auxposY, this);
-			//control.anadir(posX, posY, this);
 
-			//cout << "Pájaro id: " << this << " X = " << posX << " Y = " << posY << " Ang = " << angulo  << endl;
-			//sleep(1);
+			//Programar toda la interaccion entre las aves 
+			uBarrier x(CANTIDAD);
 
-			usleep(5 * 1000000);
+			auxposX = posX;
+			auxposY = posY;
+			auxAngulo = angulo;
+			auxaceleracion = aceleracion;
+			auxvelocidad = velocidad;
+
+			uBarrier y(CANTIDAD);
+
 		}
 	};
 
 
 public:	
-	float auxposX, auxposY, auxAngulo;
-	Pajaro(float posX, float posY, float angulo) : posX(posX), posY(posY), angulo(angulo) {};
+	float auxposX, auxposY, auxAngulo, auxaceleracion, auxvelocidad;
+	Pajaro(float posX, float posY, float angulo, float velocidad) : posX(posX), posY(posY), angulo(angulo), velocidad(velocidad) {};
 	~Pajaro(){}; 
 };
 
@@ -148,32 +90,29 @@ void setup()
     gluOrtho2D(0, EJEX, 0, EJEY);
 }
 
+void draw (float auxposX, float auxposY, float auxAngulo)
+{
+	glColor3f(1, 1, 1); // blanco
+	glTranslatef(auxposX, auxposY, 0);
+	glRotatef(-auxAngulo, 0, 0, 1);
+	glBegin(GL_TRIANGLES);			
+		glVertex3f(-2.5, -4.841225, 0); // Primer vertice
+	    glVertex3f( 2.5, -4.841225, 0); // Segundo vertice
+	    glVertex3f( 0,  4.841225, 0); // Tercer vertice     
+	glEnd();
+	glRotatef(auxAngulo, 0, 0, 1);
+	glTranslatef(-auxposX, -auxposY, 0);
+}
+
 
 void display(){
-	
-	
-
     while(1)
     {
     	
-
 	    glClear(GL_COLOR_BUFFER_BIT);
-	    glColor3f(1, 0, 0);
-
 	    //cout << bandada[0]->auxposX << " " << bandada[0]->auxposY;
 	    for (int i = 0; i < CANTIDAD; ++i)
-	    {
-	    	glColor3f(1, 1, 1); // blanco
-			glTranslatef(bandada[i]->auxposX, bandada[i]->auxposY, 0);
-		    glRotatef(bandada[i]->auxAngulo, 0, 0, 1);
-		    glBegin(GL_TRIANGLES);			
-				glVertex3f(-2.5, -4.841225, 0); // Primer vertice
-	            glVertex3f( 2.5, -4.841225, 0); // Segundo vertice
-	            glVertex3f( 0,  4.841225, 0); // Tercer vertice     
-		    glEnd();
-			glRotatef(-bandada[i]->auxAngulo, 0, 0, 1);
-			glTranslatef(-bandada[i]->auxposX, -bandada[i]->auxposY, 0);
-		}
+		    draw(bandada[i]->auxposX, bandada[i]->auxposY, bandada[i]->auxAngulo);
 	    glFlush();
     }
 }
@@ -192,7 +131,9 @@ void uMain::main()
 	//Mapa *cielo = new Mapa();
 	
 	for(int i = 0; i < CANTIDAD; ++i)
-		bandada.push_back(new Pajaro(rand() % EJEX, rand() % EJEY, rand() % 360));
+		bandada.push_back(new Pajaro(rand() % EJEX, rand() % EJEY, rand() % 360, ((rand() % 100)+1)/10000.0 ));
 	setup();
 	glutMainLoop();
 }
+
+//
